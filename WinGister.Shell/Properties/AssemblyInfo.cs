58d4fdedf977ac0cc19fb0828204cd71804e5ed2
Luke Wyatt <luke.a.wyatt@gmail.com>
2022-04-28 22:36:49 -0400

    Assembly updates

--- WinGister.Shell/Properties/AssemblyInfo.cs
+++ WinGister.Shell/Properties/AssemblyInfo.cs
@@ -2,35 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("WinGister.Shell")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("WinGister - Windows Shell Gist Explorer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("WinGister")]
 [assembly: AssemblyProduct("WinGister.Shell")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2022 Luke Wyatt")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(true)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2e7eee14-d475-4e29-b140-afd213e01171")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
